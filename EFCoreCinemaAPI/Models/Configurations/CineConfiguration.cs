b3cceed325516543bfbed44408f134a7ec3e90d1
Elmer Marquez <leomarqz2020@gmail.com>
2025-06-14 14:11:28 -0600

    relacion uno a uno con EF Core

--- EFCoreCinemaAPI/Models/Configurations/CineConfiguration.cs
+++ EFCoreCinemaAPI/Models/Configurations/CineConfiguration.cs
@@ -11,6 +11,12 @@ namespace EFCoreCinemaAPI.Models.Configurations
             builder.Property(c => c.Name)
                 .HasMaxLength(150)
                 .IsRequired();
+
+            //relacion uno a uno con CineOffer
+            builder.HasOne(c => c.CineOffer) //un cine tiene una oferta
+                .WithOne() //primera forma, ademas se usa asi si no se crease una propiedad de navegacion en CineOffer
+                           //.WithOne(co => co.Cine) //una oferta tiene un solo cine
+                .HasForeignKey<CineOffer>(co => co.CineId); //la clave foranea de CineOffer es CineId
         }
     }
 }
