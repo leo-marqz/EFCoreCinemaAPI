8803b1ffabbf872d6c8c93a1db55f6960fb6072b
Elmer Marquez <leomarqz2020@gmail.com>
2025-06-14 14:18:09 -0600

    Configuracion de relaciones mucho a muchos en el API Fluent

--- EFCoreCinemaAPI/Models/Configurations/CineConfiguration.cs
+++ EFCoreCinemaAPI/Models/Configurations/CineConfiguration.cs
@@ -17,6 +17,13 @@ namespace EFCoreCinemaAPI.Models.Configurations
                 .WithOne() //primera forma, ademas se usa asi si no se crease una propiedad de navegacion en CineOffer
                            //.WithOne(co => co.Cine) //una oferta tiene un solo cine
                 .HasForeignKey<CineOffer>(co => co.CineId); //la clave foranea de CineOffer es CineId
+
+            //si se usan las convenciones de EF Core,
+            //no es necesario hacer esto, pero es una buena practica
+            //prodiedad de navegacion, propiedad de llave primaria y clave foranea
+            builder.HasMany(c=>c.CineRooms) //un cine tiene muchas salas
+                .WithOne(cr => cr.Cine) //una sala pertenece a un cine
+                .HasForeignKey(cr => cr.CineId); //la clave foranea de CineRoom es CineId
         }
     }
 }
--- EFCoreCinemaAPI/Models/Configurations/MovieActorConfiguration.cs
+++ EFCoreCinemaAPI/Models/Configurations/MovieActorConfiguration.cs
@@ -11,6 +11,16 @@ namespace EFCoreCinemaAPI.Models.Configurations
             builder.HasKey(ma => new { ma.MovieId, ma.ActorId });
             builder.Property(ma => ma.Character)
                 .HasMaxLength(150);
+
+            //relaciones de muchos a muchos
+
+            builder.HasOne(ma=>ma.Actor)
+                    .WithMany(a => a.MoviesActors) //un actor puede tener muchas peliculas
+                    .HasForeignKey(ma => ma.ActorId); //la clave foranea de MovieActor es ActorId
+
+            builder.HasOne(ma => ma.Movie)
+                    .WithMany(m => m.MoviesActors) //una pelicula puede tener muchos actores
+                    .HasForeignKey(ma => ma.MovieId); //la clave foranea de MovieActor es MovieId
         }
     }
 }
