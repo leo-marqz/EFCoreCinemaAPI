524a9809d364e30edf357e8a783677e66bb4f23d
leonel Marquez <leomarqz2020@gmail.com>
2025-07-05 16:09:36 -0600

    Iniciando con las pruebas unitarias con user service.

---
+++ EFCoreCinemaAPI.Test/UserServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using EFCoreCinemaAPI.Services;
+
+namespace EFCoreCinemaAPI.Test
+{
+    [TestClass]
+    public sealed class UserServiceTests
+    {
+        //Estructura del Nombre del Test: Metodoa Probar _ Lo que se Espera _ Test
+
+        [TestMethod]
+        public void GetUserId_NotNullOrEmpty_Test()
+        {
+            // Prepare
+            var userService = new UserService();
+
+            // test
+            var userId = userService.GetUserById();
+
+            // verify
+            Assert.IsNotNull(userId, "User ID should not be null.");
+            Assert.AreNotEqual(string.Empty, userId, "User ID should not be an empty string.");
+        }
+    }
+}
